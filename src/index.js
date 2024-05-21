84d935fcdefaff536c707fe67439bcaf232c77bb
Assaf <1214assaf@gmail.com>
2024-05-21 11:25:40 +0300

    refactor: Update index.js to use createRoot for rendering

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import "./index.css";
 import App from "./App";
 import store from "./store/index";
 
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById("root")
-);
+const rootElement = document.getElementById("root");
+if (rootElement) {
+  createRoot(rootElement).render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
+}
