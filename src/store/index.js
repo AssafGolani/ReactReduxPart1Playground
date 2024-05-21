193b83db1d8d4f40dc0fc6ee85610fcd84a69108
Assaf <1214assaf@gmail.com>
2024-05-21 15:02:21 +0300

    refactor: Add authentication slice to store
    This commit adds a new authentication slice to the store, allowing for user login and logout functionality. The initial state is set to isAuthenticated: false, and the login and logout actions are defined to update the isAuthenticated state accordingly.

--- src/store/index.js
+++ src/store/index.js
@@ -21,13 +21,30 @@ const counterSlice = createSlice({
   },
 });
 
+const initialAuthState = { isAuthenticated: false };
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState: initialAuthState,
+  reducers: {
+    login(state) {
+      state.isAuthenticated = true;
+    },
+    logout(state) {
+      state.isAuthenticated = false;
+    },
+  },
+});
+
 const store = configureStore({
   reducer: {
     counter: counterSlice.reducer,
+    auth: authSlice.reducer,
   },
 });
 
 export const counterActions = counterSlice.actions;
+export const authActions = authSlice.actions;
 export default store;
 
 /* const counterReducer = (state = initialState, action) => {
