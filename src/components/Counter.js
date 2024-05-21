c21b3d0d833b59b5606c3a444d80d993499d53db
Assaf <1214assaf@gmail.com>
2024-05-21 12:41:22 +0300

    refactor: Add toggle functionality to show/hide counter in Counter component

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -4,8 +4,11 @@ import classes from "./Counter.module.css";
 
 const Counter = () => {
   const counter = useSelector((state) => state.counter);
+  const showCounter = useSelector((state) => state.showCounter);
   const dispatch = useDispatch();
-  const toggleCounterHandler = () => {};
+  const toggleCounterHandler = () => {
+    dispatch({ type: "TOGGLE" });
+  };
 
   const incrementHandler = () => {
     dispatch({ type: "INCREMENT" });
@@ -22,7 +25,7 @@ const Counter = () => {
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      {showCounter && <div className={classes.value}>{counter}</div>}
       <div>
         <button onClick={incrementHandler}>INCREMENT</button>
         <button onClick={increaseHandler}>Increase by 5</button>
--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,31 @@
 import { createStore } from "redux";
-
-const counterReducer = (state = { counter: 0 }, action) => {
+const initialState = { counter: 0, showCounter: true };
+const counterReducer = (state = initialState, action) => {
   switch (action.type) {
     case "INCREMENT":
-      return { ...state, counter: state.counter + 1 };
+      return {
+        ...state,
+        counter: state.counter + 1,
+        showCounter: state.showCounter,
+      };
     case "INCREASE":
-      return { ...state, counter: state.counter + action.amount };
+      return {
+        ...state,
+        counter: state.counter + action.amount,
+        showCounter: state.showCounter,
+      };
     case "DECREMENT":
-      return { ...state, counter: state.counter - 1 };
+      return {
+        ...state,
+        counter: state.counter - 1,
+        showCounter: state.showCounter,
+      };
+    case "TOGGLE":
+      return {
+        ...state,
+        counter: state.counter,
+        showCounter: !state.showCounter,
+      };
     default:
       return state;
   }
