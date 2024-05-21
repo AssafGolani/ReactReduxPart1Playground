26dc695c0a0312d18849a6926460db793b4e7447
Assaf <1214assaf@gmail.com>
2024-05-21 11:26:01 +0300

    chore: Update Counter component to include increment and decrement buttons

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,14 +1,27 @@
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import classes from "./Counter.module.css";
 
 const Counter = () => {
   const counter = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
   const toggleCounterHandler = () => {};
+
+  const incrementHandler = () => {
+    dispatch({ type: "INCREMENT" });
+  };
+
+  const decrementHandler = () => {
+    dispatch({ type: "DECREMENT" });
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
       <div className={classes.value}>{counter}</div>
+      <div>
+        <button onClick={incrementHandler}>INCREMENT</button>
+        <button onClick={decrementHandler}>DECREMENT</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
--- src/store/index.js
+++ src/store/index.js
@@ -3,9 +3,9 @@ import { createStore } from "redux";
 const counterReducer = (state = { counter: 0 }, action) => {
   switch (action.type) {
     case "INCREMENT":
-      return state.counter + 1;
+      return { ...state, counter: state.counter + 1 };
     case "DECREMENT":
-      return state.counter - 1;
+      return { ...state, counter: state.counter - 1 };
     default:
       return state;
   }
